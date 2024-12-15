27593e14dcf6dfdcef55e8c2304eb1d0c4311544
FarciarzYT <140611697+FarciarzYT@users.noreply.github.com>
2024-12-15 17:36:44 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import FundCheckerComponent from './components/FundCheckerComponent';
+
+export default function Home() {
+    return (
+        <div className="min-h-screen  flex justify-center items-center p-4 bg-black">
+            <FundCheckerComponent />
+        </div>
+    );
+}
